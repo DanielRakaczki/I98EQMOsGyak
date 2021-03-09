6a2b42fea69b58f1181aa9a303e6ce15d5fd5bab
Daniel Rakaczki <daniel.rakaczki@gmail.com>
2021-03-09 15:47:45 +0100

    4. gyakorlat 1-2-3

---
+++ I98EQM_0309/child.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main (void) {
+int i = 0;
+
+for(i = 0; i < 5; i++){
+	printf("Rakaczki Daniel, I98EQM, GE-IK\n");
+}
+
+return 0;
+
+}
